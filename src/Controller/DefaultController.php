868cf17e60afbd13e580a5fd124a1c584241d96f
DeuklyoungKo <deutschgon@gmail.com>
2019-03-10 20:42:08 +0100

    finished chapter 1-27 of Pro Testing (https://symfonycasts.com/tracks/testing)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Enclosure;
+use App\Repository\DinosaurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,6 +23,34 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             'enclosures' => $enclosures
         ]);
+    }
+
+
+    /**
+     * @Route("/grow", name="grow_dinosaur", methods={"POST"})
+     */
+    public function growAction(Request $request, DinosaurRepository $dinosaurRepository)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $enclosure = $manager->getRepository(Enclosure::class)
+                             ->find($request->request->get('enclosure'));
+
+        $specification = $request->request->get('specification');
+        $dinosaur = $dinosaurRepository->growFromSpecification($specification);
+
+        $dinosaur->setEnclosure($enclosure);
+        $enclosure->addDinosaur($dinosaur);
+
+        $manager->flush();
+
+        $this->addFlash('success', sprintf(
+            'Grew a %s in enclosure #%d',
+            mb_strtolower($specification),
+            $enclosure->getId()
+        ));
+
+        return $this->redirectToRoute('homepage');
     }
 
 
